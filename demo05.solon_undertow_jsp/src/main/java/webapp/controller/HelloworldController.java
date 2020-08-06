c86b994bc3aaa6c5dd809c16c30c26bf6b9638ec
noear <noear@live.cn>
2020-08-06 14:37:22 +0800

    demo

---
+++ demo05.solon_undertow_jsp/src/main/java/webapp/controller/HelloworldController.java
@@ -0,0 +1,36 @@
+package webapp.controller;
+
+import org.noear.solon.annotation.XController;
+import org.noear.solon.annotation.XInject;
+import org.noear.solon.annotation.XMapping;
+import org.noear.solon.core.ModelAndView;
+import org.noear.solon.core.XContext;
+import webapp.model.UserModel;
+
+@XController
+public class HelloworldController {
+
+    @XInject("custom.user")
+    protected String user;
+
+    @XMapping("/helloworld")
+    public Object helloworld(XContext ctx){
+        UserModel m = new UserModel();
+        m.setId(10);
+        m.setName("刘之西东");
+        m.setSex(1);
+
+        ModelAndView vm = new ModelAndView("helloworld.jsp");
+
+        vm.put("title","demo");
+        vm.put("message","hello world!");
+
+        vm.put("m",m);
+
+        vm.put("user", user);
+
+        vm.put("ctx",ctx);
+
+        return vm;
+    }
+}
