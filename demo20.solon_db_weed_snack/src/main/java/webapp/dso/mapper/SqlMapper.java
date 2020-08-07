818bdb48c042e7492cc73d97b9ff47cf49d44977
noear <noear@live.cn>
2020-08-07 13:32:20 +0800

    demo

--- demo20.solon_db_weed_snack/src/main/java/webapp/dso/mapper/SqlMapper.java
+++ demo20.solon_db_weed_snack/src/main/java/webapp/dso/mapper/SqlMapper.java
@@ -14,10 +14,13 @@ import webapp.model.*;
 @Db("db2")
 @Namespace("webapp.dso.mapper")
 public interface SqlMapper{
+    //随便取条数据的ID
     int appx_get() throws Exception;
 
+    //根据id取条数据
     AppxModel appx_get2(int app_id) throws Exception;
 
+    //取一批ID
     Map<String,Object> appx_get3(String tb, int app_id) throws Exception;
 
     List<AppxModel> appx_getlist(int app_id) throws Exception;
