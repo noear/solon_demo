db95a70f06ccedb4e382876187aec9cacef0dcce
noear <noear@live.cn>
2020-08-07 14:17:05 +0800

    demo

--- demo07.solon_weed/src/main/java/webapp/DemoApp.java
+++ demo07.solon_weed/src/main/java/webapp/DemoApp.java
@@ -8,12 +8,12 @@ import org.noear.solon.XApp;
  *
  * CREATE TABLE `appx` (
  *   `app_id` int NOT NULL AUTO_INCREMENT COMMENT '应用ID',
- *   `app_key` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '应用访问KEY',
- *   `akey` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '（用于取代app id 形成的唯一key） //一般用于推广注册之类',
+ *   `app_key` varchar(40) DEFAULT NULL COMMENT '应用访问KEY',
+ *   `akey` varchar(40) DEFAULT NULL COMMENT '（用于取代app id 形成的唯一key） //一般用于推广注册之类',
  *   `ugroup_id` int DEFAULT '0' COMMENT '加入的用户组ID',
  *   `agroup_id` int DEFAULT NULL COMMENT '加入的应用组ID',
- *   `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '应用名称',
- *   `note` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL COMMENT '应用备注',
+ *   `name` varchar(50) DEFAULT NULL COMMENT '应用名称',
+ *   `note` varchar(50) DEFAULT NULL COMMENT '应用备注',
  *   `ar_is_setting` int NOT NULL DEFAULT '0' COMMENT '是否开放设置',
  *   `ar_is_examine` int NOT NULL DEFAULT '0' COMMENT '是否审核中(0: 没审核 ；1：审核中)',
  *   `ar_examine_ver` int NOT NULL DEFAULT '0' COMMENT '审核 中的版本号',
