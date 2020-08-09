268b7ef67fa7847fed0f7ea669efbeee29f92554
noear <noear@live.cn>
2020-08-09 15:44:20 +0800

    demo

---
+++ demo08.solon_mybatis/src/main/java/webapp/controller/TranController.java
@@ -0,0 +1,29 @@
+package webapp.controller;
+
+import org.noear.solon.annotation.XController;
+import org.noear.solon.annotation.XInject;
+import org.noear.solon.annotation.XMapping;
+import org.noear.solon.extend.mybatis.MybatisProxy;
+import webapp.dso.mapper.AppxMapper;
+
+@XMapping("/tran/")
+@XController
+public class TranController {
+    @XInject
+    AppxMapper appxMapper;
+
+    /**
+     * mybatis-solon-plugin 的事务，需要通过 MybatisProxy 发起
+     *
+     * solon 不目前支持注解事务，说明出于性能考虑
+     * */
+    @XInject("db1f")
+    MybatisProxy proxy;
+
+    @XMapping("test")
+    public Object test() throws Throwable{
+        return proxy.tran((s)->{
+            s.result = appxMapper.appx_get();
+        });
+    }
+}
