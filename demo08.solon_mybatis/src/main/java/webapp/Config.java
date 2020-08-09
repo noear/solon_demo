b28de8855e5e649703c582f84ba75596d2ce210a
noear <noear@live.cn>
2020-08-09 10:52:31 +0800

    demo

--- demo08.solon_mybatis/src/main/java/webapp/Config.java
+++ demo08.solon_mybatis/src/main/java/webapp/Config.java
@@ -5,16 +5,10 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.noear.solon.annotation.XBean;
 import org.noear.solon.annotation.XConfiguration;
 import org.noear.solon.annotation.XInject;
-import org.noear.solon.extend.mybatis.MapperScan;
 import org.noear.solon.extend.mybatis.MybatisAdapter;
 
-/**
- * MapperScan 的作用：
- * 扫描 @basePackages 里的类，
- * 然后 用 @sqlSessionFactoryRef 生成 mapper 实例
- * 最后 注册到 bean 管理中心
- * */
-@MapperScan(basePackages = "webapp.dso.db1", sqlSessionFactoryRef = "db1f")
+import java.util.Properties;
+
 @XConfiguration
 public class Config {
     @XBean("db1f")
@@ -22,6 +16,20 @@ public class Config {
         //
         //可以用默认的配置
         //
-        return new MybatisAdapter(dataSource).getFactory();
+        return new MybatisAdapter(dataSource)
+                .mapperScan("webapp.dso.db1")   //替代@mapperScan注解（相对来说，可以把多个 mapperScan 安排在一个 Config里）
+                .getFactory();
+    }
+
+    @XBean("db2f")
+    public SqlSessionFactory db2f(
+            @XInject("${test.db2}") HikariDataSource dataSource,
+            @XInject("${mybatis.db2f}") Properties props) {
+        //
+        //可以指定配置 ${mybatis.db2f}
+        //
+        return new MybatisAdapter(dataSource, props)
+                .mapperScan("webapp.dso.db2")
+                .getFactory();
     }
 }
