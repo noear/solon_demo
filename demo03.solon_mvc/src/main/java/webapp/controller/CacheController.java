012395be31b6cbbaf70a8ab1ac31b0f6042192bf
noear <noear@live.cn>
2020-09-04 17:42:40 +0800

    demo

--- demo03.solon_mvc/src/main/java/webapp/controller/CacheController.java
+++ demo03.solon_mvc/src/main/java/webapp/controller/CacheController.java
@@ -8,15 +8,21 @@ import java.util.Date;
 
 @XController
 public class CacheController {
-    @XCache(tags = "test" , seconds = 10)
+    @XCache(tags = "test_${label}" , seconds = 10)
     @XMapping("/cache/")
-    public Object test() {
+    public Object test(int label) {
         return new Date();
     }
 
     @XCache(clearTags = "test")
     @XMapping("/cache/clear")
     public String clear() {
+        return "清除成功(其实无效)-" + new Date();
+    }
+
+    @XCache(clearTags = "test_${label}")
+    @XMapping("/cache/clear2")
+    public String clear2(int label) {
         return "清除成功-" + new Date();
     }
 }
