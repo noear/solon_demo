d5d6f6144293f06c610b26fef081f8d74946e682
noear <noear@live.cn>
2020-09-07 11:38:16 +0800

    demo

--- demo03.solon_mvc/src/main/java/webapp/Config.java
+++ demo03.solon_mvc/src/main/java/webapp/Config.java
@@ -1,4 +1,24 @@
 package webapp;
 
+import org.noear.solon.XUtil;
+import org.noear.solon.annotation.XBean;
+import org.noear.solon.annotation.XConfiguration;
+import org.noear.solon.extend.validation.ValidatorManager;
+
+@XConfiguration
 public class Config {
+    @XBean
+    public void init() {
+        ValidatorManager.globalSet(new ValidatorManager((ctx, ano, rst, msg) -> {
+            ctx.setHandled(true);
+
+            if (XUtil.isEmpty(msg)) {
+                msg = ano.annotationType().getSimpleName() + " verification failed: " + rst.getDescription();
+            }
+
+            ctx.output(msg);
+
+            return true;
+        }));
+    }
 }
