0768d6f4b88b221b99292328e2783319bdf02f8b
noear <noear@live.cn>
2020-09-08 12:03:12 +0800

    demo

--- demo03.solon_mvc/src/main/java/webapp/controller/ValidationController.java
+++ demo03.solon_mvc/src/main/java/webapp/controller/ValidationController.java
@@ -1,17 +1,21 @@
 package webapp.controller;
 
+import lombok.NonNull;
 import org.noear.solon.annotation.XController;
 import org.noear.solon.annotation.XMapping;
+import org.noear.solon.extend.validation.annotation.NoRepeatSubmit;
 import org.noear.solon.extend.validation.annotation.NotNull;
+import org.noear.solon.extend.validation.annotation.Pattern;
 import org.noear.solon.extend.validation.annotation.XValid;
 
-@XValid
+@XValid  //为控制器开启校验能力；也可以做用在一个基类上
 @XController
 public class ValidationController {
 
-    @NotNull({"name"})
+    @NoRepeatSubmit
+    @NotNull({"name", "icon", "mobile"})
     @XMapping("/valid")
-    public void test(String name){
+    public void test(String name, String icon, @Pattern("13\\d{9}") String mobile) {
 
     }
 }
