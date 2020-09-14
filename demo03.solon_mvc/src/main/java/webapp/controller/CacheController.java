9b03de60e4a0a47f2f6a7f86a0897166e5810107
noear <noear@live.cn>
2020-09-14 13:17:02 +0800

    demo

--- demo03.solon_mvc/src/main/java/webapp/controller/CacheController.java
+++ demo03.solon_mvc/src/main/java/webapp/controller/CacheController.java
@@ -8,18 +8,27 @@ import java.util.Date;
 
 @XController
 public class CacheController {
-    @XCache(tags = "test_${label}" , seconds = 10)
+    /**
+     * 执行结果缓存10秒，并添加 test_${label} 和 test1 标签
+     * */
+    @XCache(tags = "test_${label},test1" , seconds = 10)
     @XMapping("/cache/")
     public Object test(int label) {
         return new Date();
     }
 
+    /**
+     * 执行后，清除 标签为 test  的缓存（不过，目前没有 test 的示签...）
+     * */
     @XCache(clearTags = "test")
     @XMapping("/cache/clear")
     public String clear() {
         return "清除成功(其实无效)-" + new Date();
     }
 
+    /**
+     * 执行后，清除 标签为 test_${label}  的缓存
+     * */
     @XCache(clearTags = "test_${label}")
     @XMapping("/cache/clear2")
     public String clear2(int label) {
