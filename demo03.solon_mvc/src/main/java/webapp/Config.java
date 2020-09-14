b91c695e58c276b07bd2df9494d5859323c6afd9
noear <noear@live.cn>
2020-09-14 15:22:29 +0800

    demo

--- demo03.solon_mvc/src/main/java/webapp/Config.java
+++ demo03.solon_mvc/src/main/java/webapp/Config.java
@@ -1,11 +1,19 @@
 package webapp;
 
+import com.sun.javaws.security.AppContextUtil;
 import org.noear.solon.XUtil;
 import org.noear.solon.annotation.XBean;
 import org.noear.solon.annotation.XConfiguration;
+import org.noear.solon.annotation.XInject;
+import org.noear.solon.core.CacheService;
 import org.noear.solon.extend.validation.ValidatorManager;
 import org.noear.solon.extend.validation.annotation.Date;
 import org.noear.solon.extend.validation.annotation.DateValidator;
+import webapp.dso.MemCacheService;
+import webapp.dso.NotCacheService;
+
+import javax.security.auth.login.CredentialException;
+import java.util.Properties;
 
 @XConfiguration
 public class Config {
@@ -13,4 +21,17 @@ public class Config {
     public void adapter() {
         ValidatorManager.global().register(Date.class, DateValidator.instance);
     }
+
+    /**
+     *
+     */
+    @XBean
+    public CacheService cache() {
+        return new NotCacheService();
+    }
+
+//    @XBean("cache1")
+//    public CacheService cache1(@XInject("${cache1}") Properties props) {
+//        return new MemCacheService(props);
+//    }
 }
