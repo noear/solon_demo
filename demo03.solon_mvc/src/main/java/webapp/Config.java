923f8ff55613bc54d4f31f16af9b6f035ca1cbff
noear <noear@live.cn>
2020-09-16 12:44:37 +0800

    demo

--- demo03.solon_mvc/src/main/java/webapp/Config.java
+++ demo03.solon_mvc/src/main/java/webapp/Config.java
@@ -1,11 +1,14 @@
 package webapp;
 
+import org.noear.solon.XUtil;
 import org.noear.solon.annotation.XBean;
 import org.noear.solon.annotation.XConfiguration;
 import org.noear.solon.core.CacheService;
 import org.noear.solon.extend.validation.ValidatorManager;
 import org.noear.solon.extend.validation.annotation.Date;
 import org.noear.solon.extend.validation.annotation.DateValidator;
+import org.noear.solon.extend.validation.annotation.NoRepeatSubmit;
+import org.noear.solon.extend.validation.annotation.NoRepeatSubmitValidator;
 import webapp.dso.NotCacheService;
 
 
@@ -14,6 +17,23 @@ public class Config {
     @XBean
     public void adapter() {
         ValidatorManager.global().register(Date.class, DateValidator.instance);
+
+
+        ValidatorManager.global().onFailure((ctx, ano, rst, message) -> {
+            ctx.setHandled(true);
+
+            if (XUtil.isEmpty(message)) {
+                message = new StringBuilder(100)
+                        .append("@")
+                        .append(ano.annotationType().getSimpleName())
+                        .append(" verification failed")
+                        .toString();
+            }
+
+            ctx.output(message);
+
+            return true;
+        });
     }
 
     /**
