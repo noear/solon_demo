8b29bf95bcc6547f4c54e4afd81ae5aaa0b5caf9
noear <noear@live.cn>
2020-09-18 13:17:24 +0800

    demo

--- demo03.solon_mvc/src/main/java/webapp/controller/CacheController.java
+++ demo03.solon_mvc/src/main/java/webapp/controller/CacheController.java
@@ -1,6 +1,7 @@
 package webapp.controller;
 
-import org.noear.solon.annotation.XCache;
+import org.noear.solon.annotation.XCachePut;
+import org.noear.solon.annotation.XCacheRemove;
 import org.noear.solon.annotation.XController;
 import org.noear.solon.annotation.XMapping;
 
@@ -11,7 +12,7 @@ public class CacheController {
     /**
      * 执行结果缓存10秒，并添加 test_${label} 和 test1 标签
      * */
-    @XCache(tags = "test_${label},test1" , seconds = 10)
+    @XCachePut(tags = "test_${label},test1" , seconds = 10)
     @XMapping("/cache/")
     public Object test(int label) {
         return new Date();
@@ -20,7 +21,7 @@ public class CacheController {
     /**
      * 执行后，清除 标签为 test  的缓存（不过，目前没有 test 的示签...）
      * */
-    @XCache(clearTags = "test")
+    @XCacheRemove(tags = "test")
     @XMapping("/cache/clear")
     public String clear() {
         return "清除成功(其实无效)-" + new Date();
@@ -29,7 +30,7 @@ public class CacheController {
     /**
      * 执行后，清除 标签为 test_${label}  的缓存
      * */
-    @XCache(clearTags = "test_${label}")
+    @XCacheRemove(tags = "test_${label}")
     @XMapping("/cache/clear2")
     public String clear2(int label) {
         return "清除成功-" + new Date();
