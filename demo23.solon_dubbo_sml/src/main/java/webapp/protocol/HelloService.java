592e4b5ff9f67644343b9ae3424f89ba5f60dd19
noear <noear@live.cn>
2020-09-28 13:51:14 +0800

    demo

--- demo23.solon_dubbo_sml/src/main/java/webapp/protocol/HelloService.java
+++ demo23.solon_dubbo_sml/src/main/java/webapp/protocol/HelloService.java
@@ -2,6 +2,4 @@ package webapp.protocol;
 
 public interface HelloService {
     String sayHello(String name);
-    String sayHello2(String name);
-    String sayHello3(String name);
 }
--- demo23.solon_dubbo_sml/src/main/java/webapp/server/HelloServiceImpl.java
+++ demo23.solon_dubbo_sml/src/main/java/webapp/server/HelloServiceImpl.java
@@ -7,17 +7,6 @@ import webapp.protocol.HelloService;
 public class HelloServiceImpl implements HelloService {
     @Override
     public String sayHello(String name) {
-        System.out.println("from client ：" + name);
         return "hello, " + name;
-    }
-
-    @Override
-    public String sayHello2(String name) {
-        return "hello2, " + name;
-    }
-
-    @Override
-    public String sayHello3(String name) {
-        return "hello3, " + name;
     }
 }
