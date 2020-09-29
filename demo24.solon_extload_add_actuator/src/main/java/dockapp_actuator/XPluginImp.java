1e096e63b206dad2e353f384198c7789d2131968
noear <noear@live.cn>
2020-09-29 10:39:19 +0800

    demo

--- demo24.solon_extload_add_actuator/src/main/java/dockapp_actuator/XPluginImp.java
+++ demo24.solon_extload_add_actuator/src/main/java/dockapp_actuator/XPluginImp.java
@@ -1,15 +1,14 @@
 package dockapp_actuator;
 
 import org.noear.solon.XApp;
-import org.noear.solon.core.Aop;
 import org.noear.solon.core.XPlugin;
 
 public class XPluginImp implements XPlugin {
     @Override
     public void start(XApp app) {
 
-        //loadBean，会扫描文夹；
-        //app.loadBean(XPluginImp.class);
+        //beanScan，会扫描包下所有类文件并处理；
+        //app.beanScan(XPluginImp.class);
 
         //makeBean，会直接处理类
         app.beanMake(Actuator1.class);
