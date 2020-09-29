5aba79103bd94d4109f42aa71f642f1b99116184
noear <noear@live.cn>
2020-09-29 10:52:56 +0800

    demo

--- demo24.solon_extload_add_actuator/src/main/java/dockapp_actuator/XPluginImp.java
+++ demo24.solon_extload_add_actuator/src/main/java/dockapp_actuator/XPluginImp.java
@@ -7,10 +7,10 @@ public class XPluginImp implements XPlugin {
     @Override
     public void start(XApp app) {
 
-        //beanScan，会扫描包下所有类文件并处理；
+        //beanScan，会扫描包下所有类文件并处理（范围广，慢）；
         //app.beanScan(XPluginImp.class);
 
-        //makeBean，会直接处理类
+        //makeBean，会直接处理类（快）
         app.beanMake(Actuator1.class);
     }
 }
--- demo24.solon_extload_add_http/src/main/java/dockapp_http/XPluginImp.java
+++ demo24.solon_extload_add_http/src/main/java/dockapp_http/XPluginImp.java
@@ -6,7 +6,13 @@ import org.noear.solon.core.XPlugin;
 public class XPluginImp implements XPlugin {
     @Override
     public void start(XApp app) {
-//        app.get("/",x->x.output("Hello world!"));
+        //扫描出控制器
         app.beanScan(XPluginImp.class);
+
+        //如果只有一个类，可用beanMake（快）
+        //app.beanMake(HomeController.class);
+
+        //如果随便玩玩，也可用handler风格
+        //app.get("/",x->x.output("Hello world!"));
     }
 }
