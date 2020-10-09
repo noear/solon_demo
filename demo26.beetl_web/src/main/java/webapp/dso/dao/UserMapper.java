71a257adc0bd4bae2fb28e7437d7cc0648c4b87a
noear <noear@live.cn>
2020-10-09 14:49:41 +0800

    demo

--- demo26.beetl_web/src/main/java/webapp/dso/dao/UserMapper.java
+++ demo26.beetl_web/src/main/java/webapp/dso/dao/UserMapper.java
@@ -1,9 +1,17 @@
 package webapp.dso.dao;
 
+import org.beetl.sql.mapper.annotation.Sql;
 import org.beetl.sql.mapper.annotation.SqlResource;
+import org.beetl.sql.mapper.annotation.Template;
 import webapp.model.SysUserModel;
 
 @SqlResource("user")
 public interface UserMapper {
+    @Sql("select * from sys_user where id = ?")
+    SysUserModel selectById(Integer id);
+
+    @Template("select * from sys_user where id = #{id}")
+    SysUserModel selectTemplateById(Integer id);
+
     SysUserModel userSelect();
 }
