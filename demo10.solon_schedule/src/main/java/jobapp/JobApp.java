6f9ca27bc0480a5a2c68ca177618bcd3d89a34e1
noear <noear@live.cn>
2020-10-21 14:12:24 +0800

    demo

--- demo10.solon_schedule/src/main/java/jobapp/JobApp.java
+++ demo10.solon_schedule/src/main/java/jobapp/JobApp.java
@@ -2,8 +2,10 @@ package jobapp;
 
 import org.noear.solon.XApp;
 import org.noear.solon.extend.cron4j.EnableCron4j;
+import org.noear.solon.extend.quartz.EnableQuartz;
 
 @EnableCron4j
+@EnableQuartz
 public class JobApp {
     public static void main(String[] args) {
         XApp.start(JobApp.class, args);
