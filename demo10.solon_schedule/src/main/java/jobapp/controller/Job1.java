4eb66e134d804eab3244be9012f3a65d607f9f83
noear <noear@live.cn>
2020-10-21 12:54:20 +0800

    demo

--- demo10.solon_schedule/src/main/java/jobapp/controller/Job1.java
+++ demo10.solon_schedule/src/main/java/jobapp/controller/Job1.java
@@ -2,10 +2,10 @@ package jobapp.controller;
 
 import org.noear.solon.extend.cron4j.Cron4j;
 
-@Cron4j(cron5x = "2s")
+@Cron4j(cron5x = "2s", name = "job1")
 public class Job1  implements Runnable{
     @Override
     public void run() {
-        System.out.println("我是 Job1 （2s）");
+        System.out.println("我是 Job1 （2s）；如果慢了，那是被配置给改了");
     }
 }
