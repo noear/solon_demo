a13bd3af3096a2f20da5ba77cd863747feb84414
noear <noear@live.cn>
2020-11-16 21:25:10 +0800

    demo

--- demo27.solon_hasor/src/test/java/net/hasor/solon/boot/test/BootShareHasor_2.java
+++ demo27.solon_hasor/src/test/java/net/hasor/solon/boot/test/BootShareHasor_2.java
@@ -1,8 +1,8 @@
 package net.hasor.solon.boot.test;
 import net.hasor.solon.boot.EnableHasor;
-import org.noear.solon.annotation.XImport;
+import org.noear.solon.annotation.Import;
 
-@XImport(scanPackages = { "net.hasor.test.spring.mod1" })
+@Import(scanPackages = { "net.hasor.test.spring.mod1" })
 @EnableHasor(useProperties = false)
 public class BootShareHasor_2 {
 }
