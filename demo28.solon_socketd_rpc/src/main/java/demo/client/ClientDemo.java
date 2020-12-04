b27cb2191c50f14636af14816666f4c390d510c1
noear <noear@live.cn>
2020-12-04 23:39:16 +0800

    demo

--- demo28.solon_socketd_rpc/src/main/java/demo/client/ClientDemo.java
+++ demo28.solon_socketd_rpc/src/main/java/demo/client/ClientDemo.java
@@ -8,6 +8,8 @@ public class ClientDemo {
     public static void main(String[] args) {
         Solon.start(ClientDemo.class, args);
 
+        //[客户端] 调用 [服务端] 的 rpc
+        //
         HelloRpcService rpc = SocketD.create("tcp://localhost:28080", HelloRpcService.class);
 
         System.out.println(rpc.hello("noear"));
