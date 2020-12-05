97f1c39372632d318960dce3715a604f07bc6daa
noear <noear@live.cn>
2020-12-05 22:38:04 +0800

    demo

--- demo28.solon_socketd_message/src/main/java/demo/server/ServerApp.java
+++ demo28.solon_socketd_message/src/main/java/demo/server/ServerApp.java
@@ -18,7 +18,7 @@ public class ServerApp {
             //再待一秒，或许有更多会话边中来
             Thread.sleep(1000);
 
-            SessionManager.getOpens().forEach(session -> {
+            SessionManager.websocket().getOpenSessions().forEach(session -> {
                 session.send("配置是：1");
             });
         }
