5d3146c4550da25a6aa472aa0d03053555c022ad
noear <noear@live.cn>
2020-12-14 12:12:35 +0800

    demo

--- demo28.solon_socketd_rpc/src/main/java/demo/client/ClientDemo.java
+++ demo28.solon_socketd_rpc/src/main/java/demo/client/ClientDemo.java
@@ -1,15 +1,27 @@
 package demo.client;
 
 import demo.service.HelloRpcService;
+import demo.utils.EncryptUtils;
 import org.noear.solon.Solon;
 import org.noear.solon.extend.socketd.SocketD;
 import org.noear.solon.extend.socketd.protocol.MessageProtocolCompress;
+import org.noear.solon.extend.socketd.protocol.MessageProtocolSecret;
 
 public class ClientDemo {
     public static void main(String[] args) {
         Solon.start(ClientDemo.class, args, app->{
             //SocketD 启用压缩协议（默认超过1k才进行压缩）::要与服务端配套启用
-            SocketD.setProtocol(new MessageProtocolCompress());
+            SocketD.setProtocol(new MessageProtocolSecret() {
+                @Override
+                public byte[] encrypt(byte[] bytes) throws Exception {
+                    return EncryptUtils.aesEncrypt(bytes,"pLft36Ok5zfmP6zI");
+                }
+
+                @Override
+                public byte[] decrypt(byte[] bytes) throws Exception {
+                    return EncryptUtils.aesDecrypt(bytes,"pLft36Ok5zfmP6zI");
+                }
+            });
         });
 
         //[客户端] 调用 [服务端] 的 rpc
--- demo28.solon_socketd_rpc/src/main/java/demo/server/ServerDemo.java
+++ demo28.solon_socketd_rpc/src/main/java/demo/server/ServerDemo.java
@@ -1,8 +1,10 @@
 package demo.server;
 
+import demo.utils.EncryptUtils;
 import org.noear.solon.Solon;
 import org.noear.solon.extend.socketd.SocketD;
 import org.noear.solon.extend.socketd.protocol.MessageProtocolCompress;
+import org.noear.solon.extend.socketd.protocol.MessageProtocolSecret;
 
 public class ServerDemo {
     public static void main(String[] args) {
@@ -10,7 +12,17 @@ public class ServerDemo {
             app.enableSocket(true);
 
             //SiteD 启用压缩协议（默认超过1k才进行压缩）::要与客户端配套启用
-            SocketD.setProtocol(new MessageProtocolCompress());
+            SocketD.setProtocol(new MessageProtocolSecret() {
+                @Override
+                public byte[] encrypt(byte[] bytes) throws Exception {
+                    return EncryptUtils.aesEncrypt(bytes,"pLft36Ok5zfmP6zI");
+                }
+
+                @Override
+                public byte[] decrypt(byte[] bytes) throws Exception {
+                    return EncryptUtils.aesDecrypt(bytes,"pLft36Ok5zfmP6zI");
+                }
+            });
         });
     }
 }
