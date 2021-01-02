f544694cf17d49d6aa87bfa3f525c79f5e64f468
noear <noear@live.cn>
2021-01-02 14:45:21 +0800

    1.2.14

--- demo28.solon_socketd_rpc/src/main/java/demo/client/ClientDemo.java
+++ demo28.solon_socketd_rpc/src/main/java/demo/client/ClientDemo.java
@@ -2,7 +2,6 @@ package demo.client;
 
 import demo.service.HelloRpcService;
 import demo.utils.EncryptUtils;
-import org.noear.nami.Nami;
 import org.noear.solon.Solon;
 import org.noear.solon.socketd.SocketD;
 import org.noear.solon.socketd.protocol.MessageProtocolCompress;
@@ -10,8 +9,7 @@ import org.noear.solon.socketd.protocol.MessageProtocolSecret;
 
 public class ClientDemo {
     public static void main(String[] args) {
-        Solon.start(ClientDemo.class, args, app -> {
-            app.enableHttp(false);
+        Solon.start(ClientDemo.class, args, app->{
 
             //使用压缩协议；启用压缩协议（默认超过1k才进行压缩）::要与服务端配套启用
             //SocketD.setProtocol(new MessageProtocolCompress());
@@ -22,34 +20,20 @@ public class ClientDemo {
             SocketD.setProtocol(new MessageProtocolSecret() {
                 @Override
                 public byte[] encrypt(byte[] bytes) throws Exception {
-                    return EncryptUtils.aesEncrypt(bytes, "pLft36Ok5zfmP6zI");
+                    return EncryptUtils.aesEncrypt(bytes,"pLft36Ok5zfmP6zI");
                 }
 
                 @Override
                 public byte[] decrypt(byte[] bytes) throws Exception {
-                    return EncryptUtils.aesDecrypt(bytes, "pLft36Ok5zfmP6zI");
+                    return EncryptUtils.aesDecrypt(bytes,"pLft36Ok5zfmP6zI");
                 }
             });
         });
 
         //[客户端] 调用 [服务端] 的 rpc
         //
-        int port = 8080;
-
-        HelloRpcService rpc = Nami.builder().upstream(()->"tcp://localhost:" + (20000 + port))
-                                .create( HelloRpcService.class);
+        HelloRpcService rpc = SocketD.create("tcp://localhost:28080", HelloRpcService.class);
 
         System.out.println("RPC result: " + rpc.hello("noear"));
-
-
-//        rpc = Nami.builder().upstream(()->"ws://localhost:" + (15000 + port))
-//                .create( HelloRpcService.class);
-//
-//        System.out.println("RPC result: " + rpc.hello("noear"));
-//
-//        rpc = Nami.builder().upstream(()->"http://localhost:" + (port))
-//                .create( HelloRpcService.class);
-//
-//        System.out.println("RPC result: " + rpc.hello("noear"));
     }
 }
--- demo28.solon_socketd_rpc/src/main/java/demo/server/ServerDemo.java
+++ demo28.solon_socketd_rpc/src/main/java/demo/server/ServerDemo.java
@@ -10,8 +10,6 @@ public class ServerDemo {
     public static void main(String[] args) {
         Solon.start(ServerDemo.class, args, app -> {
             app.enableSocket(true);
-            app.enableWebSocket(true);
-            app.enableWebSocketD(true);
 
             //使用压缩协议； 启用压缩协议（默认超过1k才进行压缩，可以自己改改）::要与客户端配套启用
             //SocketD.setProtocol(new MessageProtocolCompress());
