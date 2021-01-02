c81e0803bd3239f809dbdef96a39e30faac81b49
noear <noear@live.cn>
2021-01-02 15:10:21 +0800

    1.2.14

--- demo31.nami_rpc/src/main/java/demo/client/ClientDemo.java
+++ demo31.nami_rpc/src/main/java/demo/client/ClientDemo.java
@@ -2,18 +2,27 @@ package demo.client;
 
 import demo.service.HelloRpcService;
 import demo.utils.EncryptUtils;
+import org.noear.nami.Nami;
 import org.noear.solon.Solon;
 import org.noear.solon.socketd.SocketD;
 import org.noear.solon.socketd.protocol.MessageProtocolSecret;
 
 public class ClientDemo {
     public static void main(String[] args) {
-        Solon.start(ClientDemo.class, args, app-> app.enableHttp(false));
+        Solon.start(ClientDemo.class, args, app -> app.enableHttp(false));
 
         //[客户端] 调用 [服务端] 的 rpc
         //
-        HelloRpcService rpc = SocketD.create("tcp://localhost:28080", HelloRpcService.class);
+        HelloRpcService rpc;
 
+        rpc = Nami.builder().upstream(() -> "tcp://localhost:28080").create(HelloRpcService.class);
+        System.out.println("RPC result: " + rpc.hello("noear"));
+
+
+        rpc = Nami.builder().upstream(() -> "ws://localhost:8080").create(HelloRpcService.class);
+        System.out.println("RPC result: " + rpc.hello("noear"));
+
+        rpc = Nami.builder().upstream(() -> "http://localhost:8080").create(HelloRpcService.class);
         System.out.println("RPC result: " + rpc.hello("noear"));
     }
 }
--- demo31.nami_rpc/src/main/java/demo/server/HelloRpcServiceImpl.java
+++ demo31.nami_rpc/src/main/java/demo/server/HelloRpcServiceImpl.java
@@ -8,15 +8,19 @@ import org.noear.solon.core.handle.Context;
 import org.noear.solon.core.handle.MethodType;
 import org.noear.solon.socketd.SocketD;
 
-@Mapping(value = "/demoe/rpc", method = MethodType.SOCKET)
+@Mapping(value = "/demoe/rpc", method = MethodType.ALL)
 @Component(remoting = true)
 public class HelloRpcServiceImpl implements HelloRpcService {
     public String hello(String name) {
         //
-        //[服务端] 调用 [客户端] 的 rpc
+        //[服务端] 调用 [客户端] 的 rpc （socket & websocket support）
         //
-        NameRpcService rpc = SocketD.create(Context.current(), NameRpcService.class);
-        name = rpc.name(name);
+        Context ctx = Context.current();
+        if (ctx.method().equals(MethodType.SOCKET.name)
+                || ctx.method().equals(MethodType.WEBSOCKET.name)) {
+            NameRpcService rpc = SocketD.create(Context.current(), NameRpcService.class);
+            name = rpc.name(name);
+        }
 
         return "name=" + name;
     }
