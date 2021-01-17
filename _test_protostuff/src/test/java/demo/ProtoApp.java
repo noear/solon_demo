4820e3ea6309190560657cdba72a2be23be4ad9a
noear <noear@live.cn>
2021-01-17 19:30:47 +0800

    1.2.18

--- _test_protostuff/src/test/java/demo/ProtoApp.java
+++ _test_protostuff/src/test/java/demo/ProtoApp.java
@@ -7,11 +7,14 @@ import io.protostuff.Schema;
 import io.protostuff.runtime.RuntimeSchema;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ProtoApp {
     private static final Schema<App> APP_SCHEMA = RuntimeSchema.getSchema(App.class);
 
     @Test
-    public void main() {
+    public void test1() {
         //
         // 序列化
         //
@@ -29,6 +32,27 @@ public class ProtoApp {
         App app2 = APP_SCHEMA.newMessage();
         ProtobufIOUtil.mergeFrom(bytes, app2, APP_SCHEMA);
 
-        assert app2.getId() == app.getId();
+        assert app2.getId().equals(app.getId()) ;
+    }
+
+    @Test
+    public void test2() {
+        //
+        // 序列化
+        //
+        App app = new App();
+        app.setId("11");
+        app.setChannel_id("22");
+        app.setVersion(33);
+        app.setPackageName("x.x.x");
+
+        byte[] bytes = ProtostuffUtil.serializer(app);
+
+        //
+        // 反序列化
+        //
+        App app2 = ProtostuffUtil.deserializer(bytes, App.class);
+
+        assert app2.getId().equals(app.getId());
     }
 }
