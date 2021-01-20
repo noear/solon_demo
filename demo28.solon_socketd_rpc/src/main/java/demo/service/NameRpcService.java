b3702c9f2bfc5528dec8ed146f56671b7cee8393
noear <noear@live.cn>
2021-01-20 18:25:34 +0800

    1.2.23

--- demo28.solon_socketd_rpc/src/main/java/demo/service/NameRpcService.java
+++ demo28.solon_socketd_rpc/src/main/java/demo/service/NameRpcService.java
@@ -3,7 +3,7 @@ package demo.service;
 
 import org.noear.nami.annotation.NamiClient;
 
-@NamiClient(name = "localrpc",path = "/demoe/rpc/name")
+@NamiClient(name = "localrpc", path = "/demoe/rpc/name")
 public interface NameRpcService {
     String name(String name);
 }
