441290abb4610b02084660ecf07cdf905455caed
noear <noear@live.cn>
2021-01-24 15:55:55 +0800

    1.2.25

--- demo09.solon_beetlsql/src/main/java/webapp/Config.java
+++ demo09.solon_beetlsql/src/main/java/webapp/Config.java
@@ -10,7 +10,8 @@ import javax.sql.DataSource;
 
 @Configuration
 public class Config {
-    @Bean(attrs = {"dialect=mysql"}) //通过特性，指定BeetlSQL的方言
+    //@Bean(attrs = {"dialect=oracle"}) //通过特性，指定BeetlSQL的方言
+    @Bean
     public DataSource db1(@Inject("${test.db1}") HikariDataSource dataSource) {
         return dataSource;
     }
