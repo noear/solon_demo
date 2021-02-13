981cc6e0ee3dd5e8eb0519b8e8af541921cc6633
noear <noear@live.cn>
2021-02-13 15:05:39 +0800

    1.3.4

--- demo13.solon_jwt2/src/main/java/webapp/utils/JwtUtils.java
+++ demo13.solon_jwt2/src/main/java/webapp/utils/JwtUtils.java
@@ -1,42 +1,45 @@
 package webapp.utils;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import org.noear.solon.Utils;
 import webapp.model.User;
 
+import java.security.Key;
 import java.util.Date;
 
 public class JwtUtils {
     public static final String SUBJECT = "RookieLi";
-    public static final String SECRETKEY = "Rookie666"; // 密钥
 
     public static final long EXPIRE = 1000 * 60 * 60 * 24 * 7;  //过期时间，毫秒，一周
 
-    public static String getJwt(User user) {
+    public static String getJwt(User user, Key key) {
         if(user == null || user.getId() == null || user.getName() == null || user.getHeadImg() == null) {
             return null;
         }
 
         String token = Jwts.builder()
-                .setSubject(SUBJECT)
                 .claim("id", user.getId())
                 .claim("name", user.getName())
                 .claim("img", user.getHeadImg())
-                .setIssuedAt(new Date())
                 .setIssuer("solon")
+                .setAudience("app.client")
+                .setSubject(SUBJECT)
+                .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + EXPIRE))
-                .signWith(SignatureAlgorithm.HS256, SECRETKEY).compact();
+                .setId(Utils.guid())
+                .signWith(key).compact();
 
         return token;
     }
-    public static Claims chekcJwt(String token) {
+    public static Claims chekcJwt(String token, Key key) {
         try {
-            final Claims claims = Jwts.parser()
-                    .setSigningKey(SECRETKEY)
+            return Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
                     .parseClaimsJws(token)
                     .getBody();
-            return claims;
         } catch (Exception e) {
             e.printStackTrace();
         }
--- demo13.solon_jwt2/src/test/java/demo/JwtUtilTest.java
+++ demo13.solon_jwt2/src/test/java/demo/JwtUtilTest.java
@@ -1,27 +1,40 @@
 package demo;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.io.Encoders;
+import io.jsonwebtoken.security.Keys;
 import org.junit.Test;
 import webapp.model.User;
 import webapp.utils.JwtUtils;
 
+import java.security.Key;
+import java.util.Base64;
+
 public class JwtUtilTest {
+
+
     @Test
     public void testGeneJwt() {
+        Key key = buildKey();
+
         User user = new User();
         user.setId(999);
         user.setHeadImg("I'm busy");
         user.setName("Rookie");
-        String token = JwtUtils.getJwt(user);
+        String token = JwtUtils.getJwt(user, key);
 
         System.out.println(token);
     }
 
     @Test
     public void testCheck() {
+        Key key = buildKey();
+
         // 下面此 token 字符串是上面的结果生成的，每次不一样，不是写死的
-        String token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJSb29raWVMaSIsImlkIjo5OTksIm5hbWUiOiJSb29raWUiLCJpbWciOiJJJ20gYnVzeSIsImlhdCI6MTU5OTcyMzg1NiwiZXhwIjoxNjAwMzI4NjU2fQ.b83hvcgTClyGBnRipUMrvkZUgHPCe_jVfowMbMA__rk";
-        Claims claims = JwtUtils.chekcJwt(token);
+        String token = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJSb29raWVMaSIsImltZyI6IkknbSBidXN5IiwibmFtZSI6IlJvb2tpZSIsImlzcyI6InNvbG9uIiwiaWQiOjk5OSwiZXhwIjoxNjEzODA0MjA3LCJpYXQiOjE2MTMxOTk0MDd9.QUJNajtuDC3XocE4ee2mR1PwD8XZNF6CXbT5C3yzpVs";
+        Claims claims = JwtUtils.chekcJwt(token, key);
 
         if (claims != null) {
             String name = (String) claims.get("name");
@@ -33,5 +46,20 @@ public class JwtUtilTest {
         } else {
             System.out.println("非法token");
         }
+    }
+
+    public Key buildKey() {
+        String secretString = "LH1w5GoNtjG7Z4yAR4I1cmF0Vvl7g769KqCcYxLNL8c=";
+
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretString));
+    }
+
+    @Test
+    public void createKey() {
+        Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+        String secretString = Encoders.BASE64.encode(key.getEncoded());
+
+        System.out.println(secretString);
     }
 }
