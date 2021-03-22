9f1f4c6809ceb1ca4a890e8b03f08e1a6dfc2a80
noear <noear@live.cn>
2021-03-22 11:31:40 +0800

    1.3.15

--- demo03.solon_mvc/src/main/java/webapp/controller/CacheByKeyController3.java
+++ demo03.solon_mvc/src/main/java/webapp/controller/CacheByKeyController3.java
@@ -1,39 +1,45 @@
 package webapp.controller;
 
+import org.noear.solon.Utils;
 import org.noear.solon.annotation.Controller;
 import org.noear.solon.annotation.Mapping;
 import org.noear.solon.extend.data.annotation.Cache;
 import org.noear.solon.extend.data.annotation.CachePut;
 import org.noear.solon.extend.data.annotation.CacheRemove;
 
+import javax.rmi.CORBA.Util;
 import java.time.LocalDateTime;
 import java.util.Date;
 
 @Controller
 public class CacheByKeyController3 {
     /**
-     * 执行结果缓存10秒，并添加 test_${label} 和 test1 标签
+     * 执行结果缓存10秒，设定 key=test3_${label} ，并添加 tag=test3 标签（可以批量删除）
      */
-    @Cache(key = "test3_${label}", seconds = 600)
+    @Cache(key = "test3_${label}", tags = "test3", seconds = 10)
     @Mapping("/cache3/")
     public String cache(int label) {
         return LocalDateTime.now().toString();
     }
+    /**
+     * 执行后，清除 tag=test3 的所有缓存，并更新 key=test3_${label} 的缓存块
+     */
+    @CachePut(key = "test3_${label}")
+    @CacheRemove(tags = "test3")
+    @Mapping("/cache3/update")
+    public String remove(int label) {
+        return "清除成功-" + new Date();
 
-    @Cache(key = "test3", seconds = 600)
+    }
+
+
+
+
+    @Cache(key = "test3", seconds = 10)
     @Mapping("/cache3/cache2")
     public String cache2() {
         return LocalDateTime.now().toString();
     }
 
 
-    /**
-     * 执行后，清除 标签为 test_${label}  的缓存
-     */
-    @CachePut(key = "test3_${label}")
-    @CacheRemove(key = "test2")
-    @Mapping("/cache3/update")
-    public String remove(int label) {
-        return "清除成功-" + new Date();
-    }
 }
