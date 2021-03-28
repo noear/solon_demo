388577106ec078d87baa33dcda0b4f0123ee49b1
noear <noear@live.cn>
2021-03-28 12:38:19 +0800

    1.3.18

---
+++ _test_luffy/src/main/java/demo/TestApp.java
@@ -0,0 +1,16 @@
+package demo;
+
+import org.noear.solon.Solon;
+import org.noear.solon.SolonApp;
+import org.noear.solon.extend.luffy.LuffyHandler;
+
+/**
+ * @author noear 2021/3/28 created
+ */
+public class TestApp {
+    public static void main(String[] args) {
+        SolonApp app = Solon.start(TestApp.class, args);
+
+        app.all("/**", new LuffyHandler());
+    }
+}
