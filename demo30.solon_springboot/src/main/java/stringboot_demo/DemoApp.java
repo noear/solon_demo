dc5dea0669b4e842f48ded66e4c9fbe0f2125bef
noear <noear@live.cn>
2021-05-18 15:49:18 +0800

    1.4.0

--- demo30.solon_springboot/src/main/java/stringboot_demo/DemoApp.java
+++ demo30.solon_springboot/src/main/java/stringboot_demo/DemoApp.java
@@ -1,13 +1,11 @@
 package stringboot_demo;
 
-import org.noear.solon.extend.springboot.EnableSolon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author noear 2020/12/28 created
  */
-@EnableSolon
 @SpringBootApplication
 public class DemoApp {
     public static void main(String[] args) {
--- demo30.solon_springboot/src/main/java/stringboot_demo/controller/Text2Controller.java
+++ demo30.solon_springboot/src/main/java/stringboot_demo/controller/Text2Controller.java
@@ -1,12 +1,9 @@
 package stringboot_demo.controller;
 
-//import org.noear.solon.Solon;
-//import org.noear.solon.core.Aop;
+import org.noear.solon.annotation.Inject;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import stringboot_demo.dso.HelloService;
-
-import javax.annotation.Resource;
 
 /**
  * Springboot 不能使用 Solon 的容器资源
@@ -16,17 +13,12 @@ import javax.annotation.Resource;
 @RestController
 public class Text2Controller {
 
+    @Inject
     HelloService helloService;
 
     @RequestMapping("/test2")
     public String home(String msg) throws Exception {
         //使用Solon的手写特性赋值，进行懒加载
-        //
-//        if(helloService == null){
-//            helloService = Aop.get(HelloService.class);
-//        }
-//
-//        String name = Solon.cfg().get("user.name");
 
         return "springboot: " + helloService.hello(msg);
     }
