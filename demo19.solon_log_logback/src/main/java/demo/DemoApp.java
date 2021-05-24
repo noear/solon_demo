93c700149ec0e2ce19b19ed5016f6932a24ad12a
noear <noear@live.cn>
2021-05-24 12:51:27 +0800

    1.4.4

--- demo19.solon_log_logback/src/main/java/demo/DemoApp.java
+++ demo19.solon_log_logback/src/main/java/demo/DemoApp.java
@@ -4,23 +4,14 @@ import org.noear.solon.Solon;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.lang.management.ManagementFactory;
-
 /**
  * @author noear 2021/5/24 created
  */
 public class DemoApp {
     public static void main(String[] args){
-        //它先
-        String pid = ManagementFactory.getRuntimeMXBean().getName().split("@")[0];
-        System.setProperty("PID", pid);
-
-        //它后
-        Logger log = LoggerFactory.getLogger(DemoApp.class);
-
         Solon.start(DemoApp.class, args);
 
-
+        Logger log = LoggerFactory.getLogger(DemoApp.class);
         log.info("Solon 启动完成");
     }
 }
