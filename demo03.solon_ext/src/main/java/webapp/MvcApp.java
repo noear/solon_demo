99af7222acec8fae320a2c1ca1135a2fcc9ec94f
noear <noear@live.cn>
2021-05-24 18:13:02 +0800

    1.4.4

--- demo03.solon_ext/src/main/java/webapp/MvcApp.java
+++ demo03.solon_ext/src/main/java/webapp/MvcApp.java
@@ -5,5 +5,7 @@ import org.noear.solon.Solon;
 public class MvcApp {
     public static void main(String[] args) {
         Solon.start(MvcApp.class, args);
+
+        System.out.println("读取配置 demo_name："+Solon.cfg().get("demo_name","1"));
     }
 }
