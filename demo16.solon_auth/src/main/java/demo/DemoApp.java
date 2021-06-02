47273717aec970295e9a1c8b3fe952af5dee939e
noear <noear@live.cn>
2021-06-02 18:21:35 +0800

    1.4.9

--- demo16.solon_auth/src/main/java/demo/DemoApp.java
+++ demo16.solon_auth/src/main/java/demo/DemoApp.java
@@ -8,5 +8,9 @@ import org.noear.solon.Solon;
 public class DemoApp {
     public static void main(String[] args) {
         Solon.start(DemoApp.class, args);
+
+        /**
+         * 此Demo只能看，不能运行；其中，BcfClient 需要初始化数据库和缓存;
+         * */
     }
 }
