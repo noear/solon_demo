c76772b2cc92014ccb0f18a8da56c7b410d1a645
noear <noear@live.cn>
2021-06-11 08:57:56 +0800

    1.4.13

--- demo01.solon_simple/src/main/java/demo/HelloworldApp.java
+++ demo01.solon_simple/src/main/java/demo/HelloworldApp.java
@@ -9,15 +9,12 @@ import org.noear.solon.core.handle.Result;
 @Controller
 public class HelloworldApp {
     public static void main(String[] args) {
-        Solon.start(HelloworldApp.class, args);
+        Solon.start(HelloworldApp.class, args)
+                .onError(e-> e.printStackTrace());
     }
 
     @Mapping("/")
     public Object helloworld(){
-
-        Result.failure();
-
-        Result.failure(400);
         String xx= "";
         return Result.succeed(xx);
     }
