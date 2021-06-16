a39161acf506dfdd1679a36899e48d05f8afa8b7
noear <noear@live.cn>
2021-06-16 10:19:32 +0800

    1.5.2

--- demo09.solon_beetlsql/src/main/java/webapp/Config.java
+++ demo09.solon_beetlsql/src/main/java/webapp/Config.java
@@ -1,8 +1,11 @@
 package webapp;
 
 import com.zaxxer.hikari.HikariDataSource;
+import org.beetl.sql.core.SQLManager;
+import org.beetl.sql.ext.solon.Db;
 import org.noear.solon.annotation.Bean;
 import org.noear.solon.annotation.Configuration;
+import org.noear.solon.annotation.Init;
 import org.noear.solon.annotation.Inject;
 
 import javax.sql.DataSource;
@@ -14,6 +17,15 @@ public class Config {
     @Bean
     public DataSource db1(@Inject("${test.db1}") HikariDataSource dataSource) {
         return dataSource;
+    }
+
+    @Db
+    SQLManager sqlManager;
+
+
+    @Init
+    public void init(){
+        //sqlManager.addIdAutoGen();
     }
 }
 
