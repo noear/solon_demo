611a8142b54fad2d54ffbb991a4093af5b127527
noear <noear@live.cn>
2021-07-14 15:43:07 +0800

    1.5.13

--- demo10.solon_schedule/README.md
+++ demo10.solon_schedule/README.md
@@ -5,7 +5,7 @@
 <dependency>
     <groupId>org.noear</groupId>
     <artifactId>cron4j-solon-plugin</artifactId>
-    <version>1.5.13</version>
+    <version>1.5.14</version>
 </dependency>
 ```
 
--- demo10.solon_schedule/src/main/java/jobapp/controller/QuartzRun1.java
+++ demo10.solon_schedule/src/main/java/jobapp/controller/QuartzRun1.java
@@ -4,11 +4,11 @@ import org.noear.solon.extend.quartz.Quartz;
 
 import java.util.Date;
 
-@Quartz(cron7x = "200ms" ,name = "QuartzRun1")
+@Quartz(cron7x = "2s" ,name = "QuartzRun1")
 public class QuartzRun1 implements Runnable {
     @Override
     public void run() {
-        System.out.println("我是定时任务: QuartzRun1(200ms) -- " + new Date().toString());
+        System.out.println("我是定时任务: QuartzRun1(2s) -- " + new Date().toString());
         //throw new RuntimeException("异常");
         System.out.println("如果间隔太长，我可能被配置给控制了");
     }
