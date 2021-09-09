cd45b3ba1171bbdba0ccfddb85dad547809c709c
noear <noear@live.cn>
2021-09-09 18:58:26 +0800

    1.5.25

--- demo08.solon_mybatisplus/src/test/java/features/PlusTest.java
+++ demo08.solon_mybatisplus/src/test/java/features/PlusTest.java
@@ -1,5 +1,7 @@
 package features;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.solon.pagination.Page;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.noear.solon.annotation.Inject;
@@ -8,6 +10,8 @@ import org.noear.solon.test.SolonTest;
 import webapp.DemoApp;
 import webapp.dso.mapper.AppxMapper2;
 import webapp.model.AppxModel;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+
 
 /**
  * @author noear 2021/9/3 created
@@ -26,5 +30,20 @@ public class PlusTest {
 
         assert app != null;
         assert app.getAppId() == 2;
+    }
+
+    @Test
+    public void test2() {
+        Page<AppxModel> page = new Page<>(1, 10);
+        IPage<AppxModel> iPage = appxMapper2.selectPage(page, new QueryWrapper<>());
+
+
+        assert iPage != null;
+
+        System.out.println("iPage.getRecords().size(): " + iPage.getRecords().size());
+        assert iPage.getRecords().size() > 0;
+
+        System.out.println("iPage.getTotal(): " + iPage.getTotal());
+        assert iPage.getTotal() > 0;
     }
 }
