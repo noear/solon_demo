f666874adcae3ef466aed9fa3eb4c516bcaf3142
noear <noear@live.cn>
2021-10-13 14:07:59 +0800

    1.5.44

--- demo01.solon_tmp/README.md
+++ demo01.solon_tmp/README.md
@@ -7,7 +7,7 @@
 <dependency>
     <groupId>org.noear</groupId>
     <artifactId>solon.boot.jlhttp</artifactId>
-    <version>1.5.41</version>
+    <version>1.5.44</version>
 </dependency>
 ```
 
--- demo07.solon_weed/src/main/java/webapp/Config.java
+++ demo07.solon_weed/src/main/java/webapp/Config.java
@@ -1,12 +1,14 @@
 package webapp;
 
 import com.zaxxer.hikari.HikariDataSource;
+import org.noear.snack.ONode;
 import org.noear.solon.Solon;
 import org.noear.solon.annotation.Bean;
 import org.noear.solon.annotation.Configuration;
 import org.noear.solon.annotation.Inject;
 import org.noear.solon.core.Aop;
 import org.noear.weed.DbContext;
+import org.noear.weed.WeedConfig;
 
 import javax.sql.DataSource;
 
@@ -15,6 +17,16 @@ public class Config {
     @Bean
     public DataSource db1(@Inject("${test.db1}") HikariDataSource ds) {
         return ds;
+    }
+
+    @Bean
+    public void initWeed() {
+        if (Solon.cfg().isDebugMode()) {
+            //执行后打印下sql
+            WeedConfig.onExecuteAft(cmd -> {
+                System.out.println(cmd.text + "\r\n" + ONode.stringify(cmd.paramMap()));
+            });
+        }
     }
 }
 
