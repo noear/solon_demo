cbbc9f883b3b7cd98ffb541a5d3d3b50fe18e0bb
noear <noear@live.cn>
2021-10-21 15:27:49 +0800

    1.5.49

--- demo08.solon_mybatis/src/main/java/webapp/Config.java
+++ demo08.solon_mybatis/src/main/java/webapp/Config.java
@@ -9,6 +9,9 @@ import javax.sql.DataSource;
 
 @Configuration
 public class Config {
+    //
+    // 加上 typed = true 的作用：可以直接使用 @Db （不带name）
+    //
     @Bean(value = "db1", typed = true)
     public DataSource db1(@Inject("${test.db1}") HikariDataSource ds) {
        return ds;
